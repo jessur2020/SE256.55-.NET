cfa4f019acc63abbbf07e822312727b121723b07
Rosario <jarosario@labs.neit.edu>
2019-11-05 19:01:12 -0500

    lab6

---
+++ Lab6Jr/Lab6Jr/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Lab6Jr
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(" Contact", "Contact", new { controller = "Home", action = "Contact" });
+            routes.MapRoute(" About", "About", new { controller = "Home", action = "About" });
+            routes.MapRoute(" Register", "Register", new { controller = "Account", action = "Register" });
+            routes.MapRoute(" Login", "Login", new { controller = "Account", action = "Login" });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Lab6Jr/Lab6Jr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab6Jr.MvcApplication" Language="C#" %>
