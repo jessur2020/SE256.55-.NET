e76960bdf59706dd4ee2b91f61498af01b95ff1c
Rosario <jarosario@labs.neit.edu>
2019-11-12 20:24:03 -0500

    week7

---
+++ week7/Lab7jr/Lab7jr/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab7jr.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            string[] movies = { "The Wolf Of Wall Street", "Troy", "Fast And Furious", "Naturo The Last", "Spider Man" };
+            ViewBag.Movies = movies;
+            string[] myFavoriteColor = { "Blue", "Green", "Red", "White", "Grey"};
+            ViewBag.MyFavoriteColor = myFavoriteColor;
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ week7/Lab7jr/Lab7jr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab7jr.Startup))]
+namespace Lab7jr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
